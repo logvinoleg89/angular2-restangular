2bf762bb712f3e05f9a53b7e86b4f9826bfc9e10
Logvin Oleg <logvinoleg89@gmail.com>
2016-11-08 11:12:35 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Angular2-Restangular
+
+From now this component is available here: [Ng2-Restangular](https://www.npmjs.com/package/ng2-restangular)
+
+# License
+
+The MIT License
+
+**[Back to top](#table-of-contents)**
---
+++ src/ng2-restangular.module.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:member-ordering no-unused-variable */
+import {ModuleWithProviders, NgModule, Optional, SkipSelf} from '@angular/core';
+import {RESTANGULAR, RestangularFactory} from './ng2-restangular.config';
+import {Restangular} from './ng2-restangular';
+import {RestangularHttp} from './ng2-restangular-http';
+
+@NgModule({
+  providers: [Restangular]
+})
+export class RestangularModule {
+  
+  constructor(@Optional() @SkipSelf() parentModule: RestangularModule) {
+    if (parentModule) {
+      throw new Error(
+        'RestangularModule is already loaded. Import it in the AppModule only');
+    }
+  }
+  
+  static forRoot(...config): ModuleWithProviders {
+    return {
+      ngModule: RestangularModule,
+      providers: [
+        RestangularHttp,
+        {provide: RESTANGULAR, useFactory: RestangularFactory(...config)},
+      ]
+    };
+  }
+}
